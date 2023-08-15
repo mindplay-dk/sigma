652aa62f4d565c5e387e04b777fe538b52d944bc
Rasmus Schultz <rasmus@mindplay.dk>
2023-08-15 16:15:02 +0200

    perf: benchmark isolation prototype

--- benchmarks/src/json/index.ts
+++ benchmarks/src/json/index.ts
@@ -1,3 +1,5 @@
+import process from 'process'
+
 import { suite, add } from 'benny'
 
 import { handlers } from '../@helpers'
@@ -7,12 +9,28 @@ import { parse as parseParjs } from './parjs'
 import { parse as parseSigmaDefer } from './sigma'
 import { parse as parseSigmaGrammar } from './sigma-grammar'
 
+const benchmarks = {
+  'sigma:grammar': () => parseSigmaGrammar(SAMPLE),
+  'sigma:defer': () => parseSigmaDefer(SAMPLE),
+  parjs: () => parseParjs(SAMPLE)
+} as any
+
+function selectBenchmark() {
+  for (const name in benchmarks) {
+    for (const arg of process.argv) {
+      if (arg === name) {
+        return add(name, benchmarks[name])
+      }
+    }
+  }
+
+  throw new Error('no benchmark selected')
+}
+
 suite(
   'JSON :: sigma vs parjs',
 
-  add('sigma:defer', () => parseSigmaDefer(SAMPLE)),
-  add('sigma:grammar', () => parseSigmaGrammar(SAMPLE)),
-  add('parjs', () => parseParjs(SAMPLE)),
+  selectBenchmark(),
 
   ...handlers
 )
